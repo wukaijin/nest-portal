ec99fac832dc6e5a64147f9c6a7fff201bd9f528
Carlos <kaijinwooo@gmail.com>
2023-02-03 14:34:12 +0800

    fix: description

--- src/blog/article/article.service.ts
+++ src/blog/article/article.service.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Carlos
  * @Date: 2023-01-20 00:43:37
- * @LastEditTime: 2023-02-03 14:32:21
+ * @LastEditTime: 2023-02-03 14:33:55
  * @FilePath: /nest-portal/src/blog/article/article.service.ts
  * @Description:
  */
@@ -45,7 +45,7 @@ export class ArticleService {
     // })
     const cates = this.articleRepo
       .createQueryBuilder('article')
-      .select(['id', 'state', 'title'].map(key => `article.${key}`))
+      .select(['id', 'state', 'title', 'description'].map(key => `article.${key}`))
       .leftJoinAndSelect('article.category', 'category')
       .leftJoinAndSelect('article.tags', 'tag')
     if (query.title) {
@@ -99,7 +99,7 @@ export class ArticleService {
     const target = await this.findOne(id)
     const cates = await this.articleRepo
       .createQueryBuilder('article')
-      .select(['id', 'state', 'title', 'description'].map(key => `article.${key}`))
+      .select(['id', 'state', 'title'].map(key => `article.${key}`))
       .leftJoin('article.category', 'category')
       .leftJoin('article.tags', 'tag')
       .where('article.category = :category', { category: target.category.id })
